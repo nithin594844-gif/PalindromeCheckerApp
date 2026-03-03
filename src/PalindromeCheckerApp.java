3caef669c712feb7ba639676ffb5841c6eba23cd
nithin594844-gif <nithin.5948.44@gmail.com>
2026-03-03 23:08:59 +0530

    UC11 : Object-Oriented Palindrome Service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,15 +1,25 @@
+class PalindromeService{
+    String input;
+    boolean isPalindrome = true;
+    public boolean checkPalindrome(String input){
+        int start = 0;
+        int end = input.length() - 1;
+        while(start < end){
+            if(input.charAt(start) != input.charAt(end)){
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+        System.out.println("Input : " + input);
+        return isPalindrome;
+    }
+}
 public class PalindromeCheckerApp {
     public static void main(String[] args){
-            String input  = "A man a plan a canal Panama";
-            String normalized = input.toLowerCase().replaceAll("[^a-z]","");
-            boolean isPalindrome = true;
-            for(int i = 0;i < normalized.length()/2;i++){
-                if(normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)){
-                    isPalindrome = false;
-                    break;
-                }
-            }
-            System.out.println("Input : " + input);
-            System.out.println("Is Palindrome? : " + isPalindrome);
+        String input  = "racecar";
+        PalindromeService PS = new PalindromeService();
+        System.out.println("Is Palindrome? : " + PS.checkPalindrome(input));
     }
 }
